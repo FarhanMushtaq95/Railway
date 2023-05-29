8bcedc78a5bcec349e7d303d80a239e5a097e135
farhanmushtaq95 <farhanmushtaq859@gmail.com>
2023-05-29 15:06:20 +0500

    added url

--- websitepost/urls.py
+++ websitepost/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import CreatePostView, GetPostView, GetCityView, GetStateView, GetCategoryView
+from .views import CreatePostView, GetPostView, GetCityView, GetStateView, GetCategoryView, SignUpAPIView
 
 
 urlpatterns = [
