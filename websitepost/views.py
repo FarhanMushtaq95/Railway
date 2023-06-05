b8a049bd81b8f78fe049920d869a0fedcdd9b2f0
farhanmushtaq95 <farhanmushtaq859@gmail.com>
2023-06-05 12:38:44 +0500

    added auth backend

--- websitepost/views.py
+++ websitepost/views.py
@@ -2,14 +2,15 @@ from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post, Category, City, State
-from .serializers import PostSerializer, CitySerializer, CategorySerializer, StateSerializer
-from authentication.models import CustomUser as User
+from .serializers import PostSerializer, CitySerializer, CategorySerializer, StateSerializer, PostcrudSerializer, PostListSerializer
+from datetime import datetime
+from authentication.models import SocialAccount,CustomUser as User
 
 class CreatePostView(APIView):
     permission_classes = [permissions.IsAuthenticated] # Ensure user is authenticated
 
     def post(self, request):
-        serializer = PostSerializer(data=request.data)
+        serializer = PostcrudSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save() # Set the user as the currently logged in user
             return Response(serializer.data, status=201)
@@ -22,16 +23,16 @@ class GetPostView(APIView):
         # Retrieve all posts from the database
         id = request.GET.get('id',None)
         if id:
-            posts = Post.objects.filter(business_id=id)
+            posts = Post.objects.filter(business_id=id,schedule_time__lte=datetime.now())
         else:
             posts = Post.objects.all()
         # Serialize the queryset to a list of serialized data
-        serializer = PostSerializer(posts, many=True)
+        serializer = PostListSerializer(posts, many=True)
         return Response(serializer.data, status=200)
 
 
 class GetCityView(APIView):
-    permission_classes = [] # Ensure user is authenticated
+    permission_classes = [permissions.IsAuthenticated] # Ensure user is authenticated
     def get(self, request):
         # Retrieve all posts from the database
         id = request.GET.get('id',None)
@@ -45,7 +46,7 @@ class GetCityView(APIView):
 
 
 class GetStateView(APIView):
-    permission_classes = [] # Ensure user is authenticated
+    permission_classes = [permissions.IsAuthenticated] # Ensure user is authenticated
     def get(self, request):
         # Retrieve all posts from the database
         id = request.GET.get('id',None)
@@ -59,7 +60,7 @@ class GetStateView(APIView):
 
 
 class GetCategoryView(APIView):
-    permission_classes = [] # Ensure user is authenticated
+    permission_classes = [permissions.IsAuthenticated] # Ensure user is authenticated
     def get(self, request):
         # Retrieve all posts from the database
         id = request.GET.get('id',None)
@@ -76,6 +77,7 @@ class SignUpAPIView(APIView):
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
+        website = request.data.get('website')
 
         # Check if the username already exists
         if User.objects.filter(email=username).exists():
@@ -83,4 +85,5 @@ class SignUpAPIView(APIView):
 
         # Create the new user
         user = User.objects.create_user(email=username, password=password, username=username)
+        website = SocialAccount.objects.create(user=user,provider=website,access_token='access_token')
         return Response({'success': 'User created'})
