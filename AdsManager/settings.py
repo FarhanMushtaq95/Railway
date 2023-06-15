b117d9e602ad7c91062c7bfab2bde7deef8303ce
farhanmushtaq95 <farhanmushtaq859@gmail.com>
2023-06-15 21:11:30 +0500

    added uptime

--- AdsManager/settings.py
+++ AdsManager/settings.py
@@ -58,7 +58,7 @@ AUTHENTICATION_BACKENDS = ['authentication.auth_backends.WebsiteBackend','authen
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=35),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
