d6b5a44d6e011f39c10ff8fc11942c4b581b5e81
farhanmushtaq95 <farhanmushtaq859@gmail.com>
2023-06-27 17:59:24 +0500

    added changes

--- AdsManager/settings.py
+++ AdsManager/settings.py
@@ -106,6 +106,7 @@ TEMPLATES = [
     },
 ]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'book-frontend', "build", "static")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'book-frontend', "build", "static"),  # update the STATICFILES_DIRS
 )
